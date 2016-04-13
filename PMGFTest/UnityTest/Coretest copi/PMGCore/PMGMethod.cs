5463e87959b2ba66ff8e220465a3656811415b04
JohanStricker <jdestr12@student.aau.dk>
2016-04-13 16:43:36 +0200

    added copi of core test under unity program so that i dont mess up the original, when poking around... and again jsut a reminder.... folder bin and obj, got added to orignal core test folder, when i ran the test once... most likely jsut thing needed for visual studio.. linux -> windows stuff... just delete them if its a problem... since i made a copi :P

---
+++ PMGFTest/UnityTest/Coretest copi/PMGCore/PMGMethod.cs
@@ -0,0 +1,97 @@
+using System.Collections.Generic;
+
+namespace PMGF
+{
+
+	namespace PMGCore
+	{
+		public class PMGMethod
+		{
+
+            public List<PMGExecuteList> _steps = new List<PMGExecuteList>();
+
+            public bool _running;
+            public int _stepIter;
+
+            public PMGExecuteList CurrentStep = null;
+
+            public PMGValueStack _valueStack = new PMGValueStack();
+
+            // Delegate for when the method is done (for calling event, perhaps);
+            public delegate void DoneDelegate();
+
+            public DoneDelegate _onDone;
+
+
+            public PMGMethod()
+            {
+                _running = false;
+                _stepIter = 0;
+            }
+
+            public void Call()
+            {
+                // Start the method
+                _running = true;
+                _stepIter = 0;
+            }
+
+            public void Pause()
+            {
+                // Stop the method (but stay at current timestep
+                _running = false;
+            }
+
+            public void Reset()
+            {
+                // Stop and reset the method (e.g. timestep 0)
+                _running = false;
+                _stepIter = 0;
+
+                // stop potential execution in list
+                CurrentStep.StopExecution();
+
+                _valueStack = new PMGValueStack();
+            }
+
+            public void Stop()
+            {
+                // same as above)
+                Reset();
+            }
+
+
+            public virtual void TimeStep()
+            {
+                if(!_running)
+                    return;
+
+                // Go to next time step, unless done
+                if(_stepIter == _steps.Count)
+                {
+                    // We are at last step (+1) - end it
+                    ReachedEnd();
+                }
+                else
+                {
+                    // Execute next step
+                    CurrentStep = _steps[_stepIter];
+                    _steps[_stepIter].Execute();
+                    _stepIter++;
+                }
+            }
+
+            public void ReachedEnd()
+
+            {
+                // Reached end of the list of time_steps.
+                Reset();
+
+                if(_onDone != null)
+                    _onDone();
+            }
+
+		}
+	}
+
+}
